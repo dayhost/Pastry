a78e3d718589e52e62586fd890d1c15aa43c30ca
Yanjun Li <yanjunli@Yanjuns-MacBook-Pro.local>
2017-10-23 23:11:06 -0400

    update

--- pastry/lib/pastry.ex
+++ pastry/lib/pastry.ex
@@ -1,6 +1,9 @@
 defmodule Pastry do
   def main(args) do
     IO.puts "======start node======"
-    Pastry.Controller.start_nodes(10, 5)
+    [numNode, numRequests] = args
+    numNode = String.to_integer(numNode)
+    numRequests = String.to_integer(numRequests)
+    Pastry.Controller.start_nodes(numNode, numRequests)
   end
 end
